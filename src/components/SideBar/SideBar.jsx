3cde0e49d940165708c47ed24d1eb62a118c7163
Eugenio Socha - Nansen <eugenio.socha2004@gmail.com>
2025-08-06 17:15:29 -0400

    Layout sidebar

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,7 +1,12 @@
 import "./SideBar.css"
-import {Link} from "react-router-dom"
+import { useState } from "react"
+import { Link } from "react-router-dom"
+import BoxIcon from "../../assets/box.svg"
+import HomeIcon from "../../assets/home.svg"
 
 function SideBar() {
+    const [openMenu, setOpenMenu] = useState(false);
+
     return (
         <div className="d-flex flex-column flex-shrink-0 p-3 sidebar text-bg-dark shadow-lg" style={{ width: '250px', height: '100vh', backgroundColor: '#4975F0' }}>
             <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
@@ -9,15 +14,24 @@ function SideBar() {
             </a>
             <hr />
             <ul className="nav nav-pills flex-column mb-auto">
-                <li className="nav-item nav-link">
+                <li className="nav-item nav-link border-bottom">
                     <Link to="/" className="tab-link" aria-current="page">
+                        <img src={HomeIcon} className="icons"/>
                         Página Inicial
                     </Link>
                 </li>
+
                 <li className="nav-item">
-                    <Link to="/produtos" className="nav-link">
+                    <li to="/produtos" className="nav-link tab-link link-light d-flex border-bottom" onClick={() => setOpenMenu(!openMenu)} aria-expanded={openMenu}>
+                        <img src={BoxIcon} className="icons ml-2"/>
                         Produtos
-                    </Link>
+                    </li>
+                    {openMenu && (
+                        <ul className="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                            <li><Link to="/produtos" className="link-light d-block ps-4">Cadastrar</Link></li>
+                            <li><a href="#" className="link-light d-block ps-4">Listar</a></li>
+                        </ul>
+                    )}
                 </li>
                 <li>
                     <a href="#" className="nav-link text-white">
